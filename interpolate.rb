57f2baf481f1ad792f22dc96c24466ad3c62c803
BlueInt32 <simon.budin@gmail.com>
2025-06-20 06:03:34 +0200

    Clean up

--- interpolate.rb
+++ interpolate.rb
@@ -5,7 +5,7 @@ if ARGV.length < 2 then
   p 'Please provide a template file path and a data file in your args. e.g. "template.erb data.erb"'
   return
 end
-template_file = ARGV[0]
+template_file_path = ARGV[0]
 require_relative ARGV[1]
 p $data
 
@@ -14,8 +14,8 @@ p $data
 #   :coffee => { :size => "small" }
 # }
 
-file = File.open(template_file)
-template_file_content = file.read
-template = ERB.new(template_file_content, trim_mode: '-') # trim_mode '-' is necessary to allow -%> ending tags, auto-removing the EOLs from output
+template_file = File.open(template_file_path)
+template_file_content = template_file.read
+erb_template = ERB.new(template_file_content, trim_mode: '-') # trim_mode '-' is necessary to allow -%> ending tags, auto-removing the EOLs from output
 # p template_file_content
-puts template.result(binding)
+puts erb_template.result(binding)
